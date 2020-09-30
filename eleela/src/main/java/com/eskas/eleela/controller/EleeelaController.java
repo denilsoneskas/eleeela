c840cdd7f5ab1997e576ed28a031a74cda37b32b
Denilson <Denilson@DESKTOP-LJ60KUA>
2020-09-29 22:08:56 -0300

    Aula 05-10

--- eleela/src/main/java/com/eskas/eleela/controller/EleeelaController.java
+++ eleela/src/main/java/com/eskas/eleela/controller/EleeelaController.java
@@ -1,12 +1,19 @@
 package com.eskas.eleela.controller;
 
+import java.time.LocalDate;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eskas.eleela.model.Post;
 import com.eskas.eleela.service.EleeelaService;
@@ -24,4 +31,29 @@ public class EleeelaController {
 		mv.addObject("posts", posts);
 		return mv;		
 	}
+	
+	@RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+	public ModelAndView getPostDetails(@PathVariable("id") long id) {
+		ModelAndView mv = new ModelAndView("postDetails");
+		Post post = eleeelaService.findById(id);
+		mv.addObject("post", post);
+		return mv;		
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.GET)
+	public String getPostForm() {
+		return "postForm";
+	}
+	
+	@RequestMapping(value = "/newpost", method = RequestMethod.POST)
+	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
+		System.out.println(result);
+		if (result.hasErrors()) {
+			return "redirect:/newpost";
+		}
+		post.setData(LocalDate.now());
+		eleeelaService.save(post);
+		return "redirect:/posts";
+	}
+	
 }
