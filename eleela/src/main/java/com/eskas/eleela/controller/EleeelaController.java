eab0d98ac9e13d73aa31f8254f286bce8a1c6c14
Denilson <Denilson@DESKTOP-LJ60KUA>
2020-10-02 00:09:25 -0300

    Aula 11-14

--- eleela/src/main/java/com/eskas/eleela/controller/EleeelaController.java
+++ eleela/src/main/java/com/eskas/eleela/controller/EleeelaController.java
@@ -49,6 +49,7 @@ public class EleeelaController {
 	public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes) {
 		System.out.println(result);
 		if (result.hasErrors()) {
+			attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
 			return "redirect:/newpost";
 		}
 		post.setData(LocalDate.now());
